79450f94365080e6b672056a93a39b0a3833067f
Caleb Howard <choward@covermymeds.com>
2017-10-14 21:55:56 -0400

    remove filters.py

--- locallibrary/catalog/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import Book, Comic
-
-class BookListFilter(django_filters.FilterSet):
-    class Meta:
-        model = Book
-        fields = ['title' , 'author_last']
-        order_by = ['title']
-
