23dc0da0322035eb3f69f06eee80046f2f2d684b
Caleb Howard <choward@covermymeds.com>
2018-03-19 09:58:04 -0400

    Halfway through adding auth

--- weatherlog/tracker/admin.py
+++ weatherlog/tracker/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Location, Journal, Group, Cloud_cover_type, Precip_type, Date_record, Precip_record, Date_record_note
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Journal)
+admin.site.register(Group)
+admin.site.register(Cloud_cover_type)
+admin.site.register(Precip_type)
+admin.site.register(Date_record)
+admin.site.register(Precip_record)
+admin.site.register(Date_record_note)
--- weatherlog/tracker/models.py
+++ weatherlog/tracker/models.py
@@ -1,3 +1,56 @@
+import datetime
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Location(models.Model):
+    locality_name = models.CharField(max_length=255)
+    zip = models.CharField(max_length=5)
+    def __str__(self):
+      return self.locality_name
+
+class Journal(models.Model):
+    description = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    locality = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+      return self.description
+
+class Group(models.Model):
+    description = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    journal = models.ForeignKey(Journal, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+      return self.description
+
+class Cloud_cover_type(models.Model):
+    description = models.CharField(max_length=25)
+    def __str__(self):
+      return self.description
+
+class Precip_type(models.Model):
+    description = models.CharField(max_length=10)
+    def __str__(self):
+      return self.description
+
+class Date_record(models.Model):
+    log_date = models.DateTimeField(default=None, blank=True, null=True)
+    cloud_cover_type = models.ForeignKey(Cloud_cover_type, on_delete=models.SET_NULL, null=True)
+    high_temp = models.IntegerField(default=None, blank=True, null=True)
+    low_temp = models.IntegerField(default=None, blank=True, null=True)
+    journal = models.ForeignKey(Journal, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+      return self.description
+
+class Precip_record(models.Model):
+    date_record = models.ForeignKey(Date_record, on_delete=models.SET_NULL, null=True)
+    precip_type = models.ForeignKey(Precip_type, on_delete=models.SET_NULL, null=True)
+    volume_in_inches = models.FloatField(default=None, blank=True, null=True)
+    def __str__(self):
+      return str(self.volume_in_inches)
+
+class Date_record_note(models.Model):
+    date_record = models.ForeignKey(Date_record, on_delete=models.SET_NULL, null=True)
+    note = models.CharField(max_length=500)
