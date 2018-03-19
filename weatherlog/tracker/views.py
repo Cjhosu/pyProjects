d8bf8e668455fa2c716c3b9bf0a07dc7d56b4fa9
Caleb Howard <choward@covermymeds.com>
2018-03-19 11:00:08 -0400

    Working auth no signup

--- weatherlog/tracker/views.py
+++ weatherlog/tracker/views.py
@@ -8,3 +8,22 @@ def index(request):
         request,
         'index.html',
 )
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            adduser = User.objects.get(username=username)
+            adduser.email = form.cleaned_data['email']
+            adduser.first_name = form.cleaned_data['first_name']
+            adduser.last_name = form.cleaned_data['last_name']
+            adduser.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
