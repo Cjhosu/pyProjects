74b5f42bb0dbe4c5fd909e9e391900f2a6c91f13
Caleb Howard <choward@covermymeds.com>
2018-03-27 23:10:46 -0400

    Calendar view

--- weatherlog/tracker/views.py
+++ weatherlog/tracker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime, date
 from .forms import SignUpForm, AddLocationForm, CreateJournalForm, DateRecordForm
 from django.contrib.auth import login, authenticate
 from .models import User, Location, Journal, Date_record, Precip_record
@@ -8,6 +9,11 @@ from django.views import generic, View
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render_to_response
+from django.utils.html import conditional_escape as esc
+from django.utils.safestring import mark_safe
+from itertools import groupby
+from calendar import HTMLCalendar, monthrange
 # Create your views here.
 
 class IndexView(LoginRequiredMixin, View):
@@ -98,6 +104,76 @@ def CreateDateRecord(request,pk):
             )
 
 def DateRecordDetailView(request,pk):
+    daterec = get_object_or_404(Date_record, pk=pk)
+    prerec = get_object_or_404(Precip_record, date_record_id = pk)
     return render(request,
-            'tracker/date_record.html'
+            'tracker/date_record.html',
+             {'daterec' :daterec , 'prerec' :prerec},
             )
+
+class WeatherCalendar(HTMLCalendar):
+
+    def __init__(self, weather):
+        super(WeatherCalendar, self).__init__()
+        self.weather = self.group_by_day(weather)
+
+    def formatday(self, day, weekday):
+        if day != 0:
+            cssclass = self.cssclasses[weekday]
+            if date.today() == date(self.year, self.month, day):
+                cssclass += ' today'
+            if day in self.weather:
+                cssclass += ' filled'
+                body = ['<ul>']
+                for weather in self.weather[day]:
+                    body.append('<li>')
+                    body.append('High Temp: ')
+                    body.append(esc(weather.high_temp))
+                    body.append('<br>')
+                    body.append('Low Temp: ')
+                    body.append(esc(weather.low_temp))
+                    body.append('<br>')
+                    body.append('</a></li>')
+                body.append('</ul>')
+                return self.day_cell(cssclass, '%d %s' % (day, ''.join(body)))
+            return self.day_cell(cssclass, day)
+        return self.day_cell('noday', '&nbsp;')
+
+    def formatmonth(self, year, month):
+        self.year, self.month = year, month
+        return super(WeatherCalendar, self).formatmonth(year, month)
+
+    def group_by_day(self, weather):
+        field = lambda weather: weather.log_date.day
+        return dict(
+            [(day, list(items)) for day, items in groupby(weather, field)]
+        )
+
+    def day_cell(self, cssclass, body):
+        return '<td class="%s">%s</td>' % (cssclass, body)
+
+
+def calendar(request, year, month):
+    year = int(year)
+    month = int(month)
+    my_weather = Date_record.objects.order_by('log_date').filter(log_date__year=year, log_date__month=month)
+    cal = WeatherCalendar(my_weather).formatmonth(year, month)
+    lPreviousYear = year
+    lPreviousMonth = month - 1
+    if lPreviousMonth == 0:
+        lPreviousMonth = 12
+        lPreviousYear = year - 1
+    lNextYear = year
+    lNextMonth = month + 1
+    if lNextMonth == 13:
+        lNextMonth = 1
+        lNextYear = year + 1
+    lYearAfterThis = year + 1
+    lYearBeforeThis = year - 1
+    return render_to_response('tracker/calendar.html', {'calendar': mark_safe(cal),'PreviousMonth' : lPreviousMonth,
+                                                       'PreviousYear' : lPreviousYear,
+                                                       'NextMonth' : lNextMonth,
+                                                       'NextYear' : lNextYear,
+                                                       'YearBeforeThis' : lYearBeforeThis,
+                                                       'YearAfterThis' : lYearAfterThis,
+                                                   })
