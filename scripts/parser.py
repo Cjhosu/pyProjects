b523b855aa02913ac89d8096bb3ec33ea44ab1a3
Caleb Howard <choward@covermymeds.com>
2019-11-04 13:59:05 -0800

    Overwrite w functional code

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,21 +1,33 @@
 import re
 
 with open('sample.csv') as file:
+    typelist = ()
     for line in file:
         if re.match(".*table.*",line):
             key, value = line.split(":")
             print('INSERT INTO' +value , end="")
         elif re.match (".*columns.*",line):
             pairs = dict(item.split(":") for item in line.split(","))
-            vallist = list(pairs.values())
-            keylist = list(pairs.keys())
-            columns = ", ".join(keylist[1:])
+            typelist = list(pairs.values())
+            collist = list(pairs.keys())
+            columns = ", ".join(collist[1:])
             print('('+ columns +')  VALUES')
         else:
             line = line.strip()
             dlist = line.split(",")
-            data = (','.join("'" + item + "'" for item in dlist))
-            if not line == "":
-                print ("("+ data + "),")
+            zipped = zip(typelist[1:],dlist)
+            data = dict(zipped)
+            mylist = []
+            if line != "": 
+                print("(",end="")
+                for k,v in data.items():
+                    if k.strip() == "int" or k.strip() == "boolean":
+                        mylist.append(v.strip(';'))
+                    else:
+                        mylist.append("'" +v.strip(';') + "'")
+                if not re.match(".*;.*", line):
+                    print(",".join(mylist)+ "),")
+                else:
+                    print(",".join(mylist)+ ")")
             else:
                 print('\n' , end="")
