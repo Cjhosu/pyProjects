5e9eabfa69f4c8bcda6c8205df4b3c5dcd3eefe2
Caleb Howard <choward@covermymeds.com>
2019-11-07 13:37:26 -0800

    Update parser to be class based

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,33 +1,45 @@
 import re
 
+class Parser:
+
+    def tables(self,line):
+        key, value = line.split(":")
+        print('INSERT INTO' +value , end="")
+
+    def columns(self, line):
+        pairs = dict(item.split(":") for item in line.split(","))
+        self.typelist = list(pairs.values())
+        collist = list(pairs.keys())
+        columns = ", ".join(collist[1:])
+        print('('+ columns +')  VALUES')
+
+    def values(self, line):
+        line = line.strip()
+        dlist = line.split(",")
+        zipped = zip(self.typelist[1:],dlist)
+        data = dict(zipped)
+        mylist = []
+        if line != "":
+            print("(",end="")
+            for k,v in data.items():
+                if k.strip() == "int" or k.strip() == "boolean":
+                    mylist.append(v.strip(';'))
+                else:
+                    mylist.append("'" +v.strip(';') + "'")
+            if not re.match(".*;.*", line):
+                print(",".join(mylist)+ "),")
+            else:
+                print(",".join(mylist)+ ")")
+        else:
+            print('\n' , end="")
+
+
 with open('sample.csv') as file:
-    typelist = ()
+    p = Parser()
     for line in file:
         if re.match(".*table.*",line):
-            key, value = line.split(":")
-            print('INSERT INTO' +value , end="")
+            p.tables(line)
         elif re.match (".*columns.*",line):
-            pairs = dict(item.split(":") for item in line.split(","))
-            typelist = list(pairs.values())
-            collist = list(pairs.keys())
-            columns = ", ".join(collist[1:])
-            print('('+ columns +')  VALUES')
+            p.columns(line)
         else:
-            line = line.strip()
-            dlist = line.split(",")
-            zipped = zip(typelist[1:],dlist)
-            data = dict(zipped)
-            mylist = []
-            if line != "": 
-                print("(",end="")
-                for k,v in data.items():
-                    if k.strip() == "int" or k.strip() == "boolean":
-                        mylist.append(v.strip(';'))
-                    else:
-                        mylist.append("'" +v.strip(';') + "'")
-                if not re.match(".*;.*", line):
-                    print(",".join(mylist)+ "),")
-                else:
-                    print(",".join(mylist)+ ")")
-            else:
-                print('\n' , end="")
+            p.values(line)
