13b1389ea755146db952b01056c757f6512c2bf2
Cjhosu <cjhosu@gmail.com>
2023-11-07 09:41:40 -0500

    Fix shims

--- dnd_utils/venv/bin/activate.fish
+++ dnd_utils/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cjhosu/repos/pyProjects/dnd_track_manager/venv"
+set -gx VIRTUAL_ENV "/Users/cjhosu/repos/pyProjects/dnd_utils/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
